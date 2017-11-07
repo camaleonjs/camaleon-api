7e39081378e52045faa6a6b479b1b8118ca69cad
Guilherme Firmiano Goncalves <ggoncalves@dotzmkt.com.br>
2017-11-07 11:26:36 -0200

    versao 2

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+export { ApiStartup } from './api.startup';
+export { IController } from './controller.intarface';
+export { IOptions } from './options.interface';
--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var api_startup_1 = require("./api.startup");
+exports.ApiStartup = api_startup_1.ApiStartup;
